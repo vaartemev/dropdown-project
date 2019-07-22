627e699ebf9f54fd0cd7a6006ff90c081d4c32fe
Vladimir Artemev <Vladimir_Artemev@epam.com>
2019-07-22 18:04:05 +0300

    Renamed classes with BEM

--- src/handlers.js
+++ src/handlers.js
@@ -5,41 +5,48 @@ const button = document.querySelector('.button');
 const input = document.querySelector('.field__text');
 const contentList = document.querySelector('.content__list');
 
-export const getTextFromListItem = (listItems) => {
-	listItems.forEach((listItem) => {
-		listItem.addEventListener('click', function () {
-			input.value = this.innerText;
-			input.setAttribute('data-text', this.innerText);
-			contentList.classList.remove('active');
-			clearContent();
-		});
-	});
+export const getTextFromListItem = listItems => {
+  listItems.forEach(listItem => {
+    listItem.addEventListener('click', function() {
+      input.value = this.innerText;
+      input.setAttribute('data-text', this.innerText);
+      contentList.classList.remove('content__list-active');
+      clearContent();
+    });
+  });
 };
 
 export const clickOnInputField = () => {
-	document.addEventListener('click', function (e) {
-		if (e.target === input || e.target === button) {
-			if (!contentList.classList.contains('active') && !button.classList.contains('active')) {
-				data.forEach((item) => render(item.label));
-				button.innerHTML = 'Close';
-				input.value = '';
-				input.focus();
-			} else {
-				input.removeAttribute('data-text');
-				clearContent();
-			}
-		} else {
-			input.value = input.getAttribute('data-text');
-			clearContent();
-		}
-	}, true);
+  document.addEventListener(
+    'click',
+    function(e) {
+      if (e.target === input || e.target === button) {
+        if (
+          !contentList.classList.contains('content__list-active') &&
+          !button.classList.contains('button-active')
+        ) {
+          data.forEach(item => render(item.label));
+          button.innerHTML = 'Close';
+          input.value = '';
+          input.focus();
+        } else {
+          input.removeAttribute('data-text');
+          clearContent();
+        }
+      } else {
+        input.value = input.getAttribute('data-text');
+        clearContent();
+      }
+    },
+    true
+  );
 };
 
 window.onscroll = () => {
-	input.value = input.getAttribute('data-text');
-	clearContent();
+  input.value = input.getAttribute('data-text');
+  clearContent();
 };
 window.onresize = () => {
-	input.value = input.getAttribute('data-text');
-	clearContent();
+  input.value = input.getAttribute('data-text');
+  clearContent();
 };
--- src/render.js
+++ src/render.js
@@ -3,22 +3,22 @@ import { getTextFromListItem } from './handlers';
 const contentList = document.querySelector('.content__list');
 const button = document.querySelector('.button');
 
-export const render = (text) => {
-    const listItem = document.createElement('li');
-    listItem.classList.add('content__item');
-    listItem.innerHTML = text;
-	contentList.appendChild(listItem); 
-	   
-    button.classList.add('active');
-    contentList.classList.add('active');
+export const render = text => {
+  const listItem = document.createElement('li');
+  listItem.classList.add('content__item');
+  listItem.innerHTML = text;
+  contentList.appendChild(listItem);
 
-    const contentItem = document.querySelectorAll('.content__item');
-    getTextFromListItem(contentItem);
+  button.classList.add('button-active');
+  contentList.classList.add('content__list-active');
+
+  const contentItem = document.querySelectorAll('.content__item');
+  getTextFromListItem(contentItem);
 };
 
 export const clearContent = () => {
-    contentList.innerHTML = '';
-    button.innerHTML = 'Open';
-	button.classList.remove('active');
-    contentList.classList.remove('active');
+  contentList.innerHTML = '';
+  button.innerHTML = 'Open';
+  button.classList.remove('button-active');
+  contentList.classList.remove('content__list-active');
 };
